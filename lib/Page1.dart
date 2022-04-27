ef82eb341822260343e706f4bdaf5afc3a9cc990
remon <remonmizanur@gmail.com>
2022-04-28 01:25:23 +0600

    April 22

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+//import 'package:page_view/Page2.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({Key? key}) : super(key: key);
@@ -13,7 +14,9 @@ class Page1 extends StatelessWidget {
         child: Text(
           "Page 1",
           style: TextStyle(
-              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25),
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
         ),
       ),
     );
